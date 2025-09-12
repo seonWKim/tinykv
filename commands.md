f8859ae6d648722fb431d61b482b8d8745f89df6
Kim Seon Woo <69591622+seonwoo960000@users.noreply.github.com>
2025-09-13 00:02:39 +0900

    Add comments on how to implement standalone_storage.go

--- commands.md
+++ commands.md
@@ -3,3 +3,6 @@
 - open terminal: :termianl
 - focus navigation tab: Ctrl + w + w 
 - move between tab, windows: Ctrl + hjlk
+- split window vertically : :vsp 
+- scroll up or down : Ctrl + u/d 
+
--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -6,6 +6,8 @@ import (
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
+// StandAloneStorage is a simple wrapper around badger 
+
 // StandAloneStorage is an implementation of `Storage` for a single-node TinyKV instance. It does not
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
@@ -27,8 +29,14 @@ func (s *StandAloneStorage) Stop() error {
 	return nil
 }
 
+// Reader shoul dsupport GET and SCAN operations 
+// Use badger.Txn so that the transaction handler provided by the badger could provide a consistent snapshot of the keys and values 
+// badger by default does not support column families. Instead, we should use engine_util to support column families
+// Don't forget to call Discard() for badger.Txn and close all iterators before discardin
+
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
+	
 	return nil, nil
 }
 
