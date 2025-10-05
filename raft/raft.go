1764a5cbd6eac6223945ce3b7b649d87b43daa5b
Kim Seon Woo <69591622+seonwoo960000@users.noreply.github.com>
2025-10-05 23:13:15 +0900

    Solve TestSingleNodeCommit2AB

--- raft/raft.go
+++ raft/raft.go
@@ -649,6 +649,10 @@ func (h *ProposeHandler) Handle(r *Raft, m pb.Message) error {
 		r.sendAppend(to)
 	}
 
+	// TODO: check whether it's okay to update the commit index here. Propose messages are leader's local message, so I think it's okay
+	// But to be sure, shouldn't we store the entries in r.RaftLog.Storage?  
+	r.RaftLog.committed += 1 
+	// TODO: I'm not sure whether to set applied here 
 	return nil
 }
 
